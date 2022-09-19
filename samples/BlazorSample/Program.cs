5a9bb18bbfdf63c4e807ddde34c217174b61d3af
Mackinnon Buck <mackinnon.buck@gmail.com>
2022-09-19 11:56:38 -0700

    Using smaller sample project

--- samples/BlazorSample/Program.cs
+++ samples/BlazorSample/Program.cs
@@ -1,20 +1,14 @@
-using BlazorServerSample.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
