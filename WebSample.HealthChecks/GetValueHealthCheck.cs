a74b98c61ffd020dd3a2cfa1e5344d30dc40d73f
Francesco Bonacci <fbonacci@microsoft.com>
2022-09-21 00:36:24 +0200

    Add HCs sample proj

---
+++ WebSample.HealthChecks/GetValueHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using WebSample.Shared.Processors;
+
+namespace WebSample.HealthChecks
+{
+    public class GetValuesHealthCheck : IHealthCheck
+    {
+        private readonly IValueProcessor _processor;
+
+        public GetValuesHealthCheck(IValueProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            _ = await _processor.GetValues();
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
