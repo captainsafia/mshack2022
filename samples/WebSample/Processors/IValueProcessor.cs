24ea8f98a16b010d78879bda66a079e8d79e024c
Francesco Bonacci <fbonacci@microsoft.com>
2022-09-22 23:43:41 +0100

    Remove shared projs

---
+++ samples/WebSample/Processors/IValueProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSample.Shared.Processors
+{
+    public interface IValueProcessor
+    {
+        Task<string[]> GetValues();
+        Task<string> GetValue(int id);
+        Task AddValue(string value);
+        Task UpdateValue(int id, string value);
+        Task RemoveValue(int id);
+    }
+}
